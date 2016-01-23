a48b64bc1693f9724e44f23097e69cf9b5bfe134
Nicolas Magliaro <nicolasmagliaro@gmail.com>
2016-01-23 13:35:48 +0100

    Menu display when auth

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ personalsis
 ===========
 
 Sistema de Gestion hecho en un Framework MVC y php
+
+version 0.7.1
--- widgets/models/menu.php
+++ widgets/models/menu.php
@@ -8,87 +8,91 @@ class menuModelWidget extends Model
 
     public function getMenu($menu)
     {
-        $menus['sidebar'] = array(
-            array(
-                'id' => 'seguridad',
-                'titulo' => 'Seguridad',
-                'enlace' => '#',
-                'divider' => false,
-                'imagen' => 'glyphicon-chevron-left',
-                'submenu' =>
 
-                        array(
-                            'id' => 'usuarios',
-                            'titulo' => 'Usuarios',
-                            'enlace' => BASE_URL . 'usuarios',
-                            'divider' => false,
-                            'imagen' => 'glyphicon-expand'
-                        ),
+        if(Session::get('autenticado')){
+            $menus['sidebar'] = array(
+                array(
+                    'id' => 'seguridad',
+                    'titulo' => 'Seguridad',
+                    'enlace' => '#',
+                    'divider' => false,
+                    'imagen' => 'glyphicon-chevron-left',
+                    'submenu' =>
 
-                        array(
-                            'id' => 'permisos',
-                            'titulo' => 'Permisos',
-                            'enlace' =>  BASE_URL . 'acl',
-                            'divider' => false,
-                            'imagen' => 'glyphicon-expand'
-                        ),
+                            array(
+                                'id' => 'usuarios',
+                                'titulo' => 'Usuarios',
+                                'enlace' => BASE_URL . 'usuarios',
+                                'divider' => false,
+                                'imagen' => 'glyphicon-expand'
+                            ),
 
-                        array(
-                            'id' => 'registro',
-                            'titulo' => 'Registro',
-                            'enlace' => BASE_URL . 'usuarios/registro',
-                            'imagen' => 'glyphicon-expand'
-                            )
+                            array(
+                                'id' => 'permisos',
+                                'titulo' => 'Permisos',
+                                'enlace' =>  BASE_URL . 'acl',
+                                'divider' => false,
+                                'imagen' => 'glyphicon-expand'
+                            ),
 
-            ),
+                            array(
+                                'id' => 'registro',
+                                'titulo' => 'Registro',
+                                'enlace' => BASE_URL . 'usuarios/registro',
+                                'imagen' => 'glyphicon-expand'
+                                )
 
-            array(
-                'id' => 'administracion',
-                'titulo' => 'Administracion',
-                'enlace' => '#',
-                'divider' => false,
-                'imagen' => 'glyphicon-chevron-left',
-                'submenu' =>
+                ),
 
-                        array(
-                            'id' => 'productos',
-                            'titulo' => 'Productos',
-                            'enlace' => BASE_URL . 'lotes/producto',
-                            'divider' => false,
-                            'imagen' => 'glyphicon-expand'
-                        ),
+                array(
+                    'id' => 'administracion',
+                    'titulo' => 'Administracion',
+                    'enlace' => '#',
+                    'divider' => false,
+                    'imagen' => 'glyphicon-chevron-left',
+                    'submenu' =>
 
-                        array(
-                            'id' => 'catalogos',
-                            'titulo' => 'Catalogos',
-                            'enlace' => BASE_URL . 'lotes/catalogo',
-                            'divider' => false,
-                            'imagen' => 'glyphicon-expand'
-                        ),
+                            array(
+                                'id' => 'productos',
+                                'titulo' => 'Productos',
+                                'enlace' => BASE_URL . 'lotes/producto',
+                                'divider' => false,
+                                'imagen' => 'glyphicon-expand'
+                            ),
 
-                        array(
-                            'id' => 'centros',
-                            'titulo' => 'Centros',
-                            'enlace' => BASE_URL . 'lotes/sede',
-                            'divider' => true,
-                            'imagen' => 'glyphicon-expand'
-                        ),
+                            array(
+                                'id' => 'catalogos',
+                                'titulo' => 'Catalogos',
+                                'enlace' => BASE_URL . 'lotes/catalogo',
+                                'divider' => false,
+                                'imagen' => 'glyphicon-expand'
+                            ),
+
+                            array(
+                                'id' => 'centros',
+                                'titulo' => 'Centros',
+                                'enlace' => BASE_URL . 'lotes/sede',
+                                'divider' => true,
+                                'imagen' => 'glyphicon-expand'
+                            ),
 
 
-            ),
+                ),
 
-            array(
-                'id' => 'reportes',
-                'titulo' => 'Reportes',
-                'enlace' => '#',
-                'divider' => true,
-                'imagen' => 'glyphicon-chevron-left',
-                'submenu' =>
-                        array()
-            )
+                array(
+                    'id' => 'reportes',
+                    'titulo' => 'Reportes',
+                    'enlace' => '#',
+                    'divider' => true,
+                    'imagen' => 'glyphicon-chevron-left',
+                    'submenu' =>
+                            array()
+                )
 
-        );
-
+            );
+        }else{
+            $menus['sidebar'] = array();
+        }
 
         $menus['top'] = array(
             array(
@@ -117,28 +121,31 @@ class menuModelWidget extends Model
         
         );
 
-         $menus['nav'] = array(
-            array(
-                'id' => 'usuarios',
-                'titulo' => 'Usuarios',
-                'enlace' => BASE_URL . 'usuarios',
-                'imagen' => 'icon-user'
-                ),
-            array(
-                'id' => 'registro',
-                'titulo' => 'Registro',
-                'enlace' => BASE_URL . 'usuarios/registro',
-                'imagen' => 'icon-user'
-                ),
-            
-            array(
-                'id' => 'acl',
-                'titulo' => 'Listas de control de acceso',
-                'enlace' => BASE_URL . 'acl',
-                'imagen' => 'icon-list-alt'
-                )
-        );
-        
+        if(Session::get('autenticado')){
+            $menus['nav'] = array(
+                array(
+                    'id' => 'usuarios',
+                    'titulo' => 'Usuarios',
+                    'enlace' => BASE_URL . 'usuarios',
+                    'imagen' => 'icon-user'
+                    ),
+                array(
+                    'id' => 'registro',
+                    'titulo' => 'Registro',
+                    'enlace' => BASE_URL . 'usuarios/registro',
+                    'imagen' => 'icon-user'
+                    ),
+                
+                array(
+                    'id' => 'acl',
+                    'titulo' => 'Listas de control de acceso',
+                    'enlace' => BASE_URL . 'acl',
+                    'imagen' => 'icon-list-alt'
+                    )
+            );
+        }else{
+            $menus['nav'] = array();
+        }
         if(Session::get('autenticado')){
             $menus['top'][0]['submenu'] = array(
                 'id' => 'cerrar',
